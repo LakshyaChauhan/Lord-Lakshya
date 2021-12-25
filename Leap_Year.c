e0dafd72fca647e82c1f5bc7a13a2be48b3f7174
LakshyaChauhan <95976218+LakshyaChauhan@users.noreply.github.com>
2021-12-25 12:35:30 +0530

    Update Leap_Year.c

--- Leap_Year.c
+++ Leap_Year.c
@@ -5,8 +5,7 @@ int main()
     int yr;
     printf("Year:-");
     scanf("%d",&yr);
-    if (yr%4==0)
-        printf("Leap Year");
-    else
-        printf("Not a Leap year");
+    yr%100==0 ?(year%400 ==0 ? printf("Leap Year\n"): printf("Not leap year\n")):
+    (year%4 ==0 ? printf("Leap Year\n"): printf("Not leap Year\n"));
+    return 0;
 }
